5ff317017a5e08058c27e0b7b17f691fb325af11
Tiago Heineck <tiagovda@gmail.com>
2026-06-16 10:02:22 -0300

    incluir varinha

---
+++ src/Varinha.java
@@ -0,0 +1,28 @@
+public class Varinha {
+
+	private int comprimento;
+	private String tipoMadeira;
+	
+	public Varinha(int comprimento, String tipoMadeira) {
+		this.comprimento = comprimento;
+		this.tipoMadeira = tipoMadeira;
+	}
+	
+	public int getComprimento() {
+		return comprimento;
+	}
+	
+	public void setComprimento(int comprimento) {
+		this.comprimento = comprimento;
+	}
+	
+	public String getTipoMadeira() {
+		return tipoMadeira;
+	}
+	
+	public void setTipoMadeira(String tipoMadeira) {
+		this.tipoMadeira = tipoMadeira;
+	}
+	
+
+}
